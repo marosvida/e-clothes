c912700a8d6b7cd9f28c3a470b27fbc9fb0f6dad
Maroš Vida <maros.vida@student.tuke.sk>
2020-08-21 11:50:35 +0200

    add routing, menu-item cmp with HOC withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+
 import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.components'
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route  exact path='/' component={HomePage} />
+      <Route  path='/hats' component={HatsPage} />
+    </Switch>
   );
 }
 
